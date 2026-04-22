3999779e8cd7a6e9087ab053541df9f28b4b0910
UsernameJJJ0 <chickenburgerfilms@gmail.com>
2026-04-22 19:04:05 -0400

    new update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+
     private Rigidbody rb;
 
     void Start()
@@ -10,10 +11,13 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        Vector2 movement = new Vector2(moveHorizontal, 0f);
-        rb.velocity = movement * speed;
+        float moveX = Input.GetAxis("Horizontal"); // A/D
+        float moveZ = Input.GetAxis("Vertical");   // W/S
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
+
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
