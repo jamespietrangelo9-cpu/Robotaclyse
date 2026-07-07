531e2e6e3a786651d1010e9e23ed7576de6c174a
UsernameJJJ0 <chickenburgerfilms@gmail.com>
2026-07-07 18:56:25 -0400

    walking animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public bool isMoveable;
+
     private Rigidbody rb;
+
+    public Animator animator;
 
     void Start()
     {
@@ -15,13 +18,22 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         if (isMoveable == true)
-        { 
+        {
             float moveX = Input.GetAxis("Horizontal"); // A/D
             float moveZ = Input.GetAxis("Vertical");   // W/S
 
             Vector3 movement = new Vector3(moveX, 0f, moveZ);
 
             rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+            if (movement.magnitude > 0.01f)
+            {
+                animator.Play("Robert-Move");
+            }
+            else
+            {
+                animator.Play("Robert-Idle");
+            }
         }
     }
 }
